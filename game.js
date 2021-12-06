283a1b338a3367b6d3e0b5ddb2e42b9cc65a514d
tanishk15sharma <tanishk1582002@gmail.com>
2021-12-07 00:08:28 +0530

    initial commit

---
+++ game.js
@@ -0,0 +1,5 @@
+const holes = document.querySelectorAll(".hole");
+const moles = document.querySelectorAll(".mole");
+const scoreBoard = document.querySelector(".score")
+// console.log(scoreBoard);
+
