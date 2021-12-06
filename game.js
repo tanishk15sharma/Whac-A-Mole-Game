61b7581edb6ccdaaa06935046276ace85af820ba
tanishk15sharma <tanishk1582002@gmail.com>
2021-12-07 01:01:46 +0530

    feat app

--- game.js
+++ game.js
@@ -2,4 +2,15 @@ const holes = document.querySelectorAll(".hole");
 const moles = document.querySelectorAll(".mole");
 const scoreBoard = document.querySelector(".score")
 // console.log(scoreBoard);
+let lastHole ;
 
+function randomTime(min,max){
+    return Math.round(Math.random() * (max -min) + min   );
+}
+
+function randomHole(holes){
+    const index = Math.floor (Math.random()  * holes.length );
+    const hole = holes[index];                             //but sometimes same hole will come 
+    console.log(hole);
+}
+// randomHole()
